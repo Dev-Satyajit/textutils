5ede3649776d344b9e1c3b13ec1a3a2dc8775117
satyajitsinha1919 <90083670+satyajitsinha1919@users.noreply.github.com>
2021-09-21 10:39:54 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./components/About";
 
 function App() {
   return (
     <>
-      <Navbar title="TextUtils" />
-      <TextForm heading="Enter your text here" />
-      <Footer />
+      <Router>
+        <Navbar title="TextUtils" />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <TextForm heading="Enter your text here" />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </>
   );
 }
